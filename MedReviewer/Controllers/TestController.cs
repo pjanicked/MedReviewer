540110d666f1cce3c9c36cb37ead49af977f061a
pjanicked <aekkim.13@gmail.com>
2020-03-13 00:32:08 +0530

    Add project files.

---
+++ MedReviewer/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedReviewer.Controllers
+{    
+    public class TestController : Controller
+    {
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
