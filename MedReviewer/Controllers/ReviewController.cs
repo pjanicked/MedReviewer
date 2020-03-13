1807d7f2f88878305c56522f41337f3e27d2ab3b
pjanicked <aekkim.13@gmail.com>
2020-03-14 00:07:05 +0530

    Datatable layout with cutom buttons

---
+++ MedReviewer/Controllers/ReviewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedReviewer.Controllers
+{
+    public class ReviewController : Controller
+    {
+        [HttpGet]
+        [Route("/Review/FindYourMed")]
+        public ActionResult FindYourMed()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TestData()
+        {
+            return Json(new
+            {
+                MedicineName = "A",
+                MedicineType = "B",
+                MedicineQuantity = "C",
+                MedicinePrice = 23
+            }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
